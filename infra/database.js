e42d1c7839eb98e19bde6057ea08a600d6d57c85
Juan Versolato Lopes <juan.versolato@outlook.com>
2024-03-20 11:44:51 -0300

    handles database ssl connection.

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,16 @@ const query = async (queryObject) => {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     client.end();
   }
